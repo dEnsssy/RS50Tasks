4d75316035987b5977805de09e80f4ddcf20f54e
dEnsssy <kravchuk.n.o@gmail.com>
2023-07-02 23:37:57 +0300

    Task03+04+05+06

---
+++ Task03.js
@@ -0,0 +1,7 @@
+const productName = "Огурец",
+  productPrice = 100,
+  productQuantity = 10,
+  resultCost = 1000;
+console.log(
+  `Вы купили ${productName} (${productQuantity} шт.) по ${productPrice} ₽. Всего было потрачено: ${resultCost} ₽`
+);
